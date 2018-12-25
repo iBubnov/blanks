69639c66f41a48a073fa3ae8361cb394acc851ab
iBubnov <illiabubnov@gmail.com>
2018-12-25 15:02:26 +0300

    add comment

--- Простой аккордеон jQuery/js/common.js
+++ Простой аккордеон jQuery/js/common.js
@@ -1,10 +1,9 @@
 $(document).ready(function(){
-
+	// Начало аккордеона
 	$('.accordion-title').next().hide();
 		$('.accordion-title').click(function(){
 		$(this).next().slideToggle();
 		$('.accordion-title').not(this).next().stop(true,true).slideUp();
 	});
-
-
+	// Конец аккордеона
 });
